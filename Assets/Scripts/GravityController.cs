7f1a78293cf4ee8b04bebf84088b2868fb80fbbf
kasamatsu <kasamatsu.shoji@gmail.com>
2022-01-26 12:39:25 +0900

    P119まで作成

---
+++ Assets/Scripts/GravityController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour //MonoBehaviorクラスを継承
+{
+    const float Gravity = 9.81f;
+
+    public float gravityScale = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("OK");
+    }
+
+    // Update is called once per frame (約1秒間に60回)
+    void Update()
+    {
+        //Debug.Log("in");
+        Vector3 vector = new Vector3();
+
+        vector.x = Input.GetAxis("Horizontal");
+        vector.z = Input.GetAxis("Vertical");
+
+        if(Input.GetKey("z"))
+        {
+            vector.y = 1.0f;
+        }
+        else
+        {
+            vector.y = -1.0f;
+        }
+
+        Physics.gravity = Gravity * vector.normalized * gravityScale;
+        
+    }
+}
